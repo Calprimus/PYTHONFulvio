95dfc110f45da931189fd4376eefa1e369dd97a6
Calprimus <fulvio.famularo@hotmail.com>
2018-04-22 17:14:59 +0200

    Codecademy Python

---
+++ python-3-playlist/codecademy_test1.py
@@ -0,0 +1,38 @@
+# fish_in_clarks_pond = 50
+
+# print ("Catching fish")
+# print (fish_in_clarks_pond)
+
+# number_of_fish_caught = 10
+# fish_in_clarks_pond = fish_in_clarks_pond - number_of_fish_caught
+# print (fish_in_clarks_pond)
+
+# january_to_june_rainfall = 1.93 + 0.71 + 3.53 + 3.41 + 3.69 + 4.50
+# annual_rainfall = january_to_june_rainfall
+
+# july_rainfall = 1.05
+# annual_rainfall += july_rainfall
+
+# august_rainfall = 4.91
+# annual_rainfall += august_rainfall
+
+# september_rainfall = 5.16
+# october_rainfall = 7.20
+# november_rainfall = 5.06
+# december_rainfall = 4.06
+
+# september_to_december_rainfall = 5.16 + 7.20 + 5.06 + 4.06
+
+# annual_rainfall += september_to_december_rainfall
+
+# print (f'the annual rainfall is : {annual_rainfall:.3f}')
+
+cucumbers = 100
+num_people = 6
+
+whole_cucumbers_per_person = cucumbers / num_people
+print (whole_cucumbers_per_person)
+
+float_cucumbers_per_person = cucumbers/num_people
+print (float_cucumbers_per_person)
+
