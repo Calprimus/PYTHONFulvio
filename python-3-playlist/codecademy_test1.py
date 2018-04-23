e0aab7788dbd633fb0c5765c51a2abf26414d4c0
Fulvio Famularo <fulvio.famularo@nbloc.co>
2018-04-23 16:28:14 +0200

    from the office

--- python-3-playlist/codecademy_test1.py
+++ python-3-playlist/codecademy_test1.py
@@ -36,8 +36,35 @@
 # float_cucumbers_per_person = cucumbers/num_people
 # print (float_cucumbers_per_person)
 
-address_string = """136 Whowho Rd
-Apt 7
-Whosville, WZ 44494"""
-print (address_string)
+# address_string = """136 Whowho Rd
+# Apt 7
+# Whosville, WZ 44494"""
+# print (address_string)
 
+"""plan your trip"""
+
+def hotel_cost(nights):
+  return 140 * nights
+
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    return 475
+
+def rental_car_cost(days):
+  cost = days * 40
+  if days >= 7:
+    cost -= 50
+  elif days >= 3:
+    cost -= 20
+  return cost
+
+def trip_cost(city, days, spending_money):
+  return rental_car_cost(days)+hotel_cost(days -1)+plane_ride_cost(city)+spending_money
+
+print (trip_cost("Los Angeles", 5, 600))
