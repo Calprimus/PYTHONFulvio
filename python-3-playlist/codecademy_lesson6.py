94a3da377b1373a5e57cb725135b6c84079b0fe0
Fulvio Famularo <fulvio.famularo@nbloc.co>
2018-04-25 13:38:04 +0200

    iterim 2

--- python-3-playlist/codecademy_lesson6.py
+++ python-3-playlist/codecademy_lesson6.py
@@ -68,8 +68,23 @@ def get_average (student):
   tests = 0.6 * average(student["tests"])
   return homework + quizzes + tests
 
-stdres = get_average(alice)
-print(f'num is {stdres:.4}')
+# stdres = get_average(alice)
+# print(f'num is {stdres:.4}')
 
-print(f'{get_average(alice):.4}')
+# print(f'{get_average(alice):.4}')
+
+def get_letter_grade (score):
+  if score >= 90:
+    return "A"
+  elif score >= 80:
+    return "B"
+  elif score >= 70:
+    return "C"
+  elif score >= 60:
+    return "D"
+  else:
+    return "F"
+  
+
+print(get_letter_grade(get_average(lloyd)))
 
