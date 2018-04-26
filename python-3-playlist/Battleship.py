8de38a28fbbc38ecdfbb0fc07f51d2cdaeb501ef
Calprimus <fulvio.famularo@hotmail.com>
2018-04-26 18:23:54 +0200

    battleship

---
+++ python-3-playlist/Battleship.py
@@ -0,0 +1,11 @@
+board = []
+
+for n in range(5):
+    board.append(["O"]*5)
+for z in board:
+    for y in z:
+        def print_board(board_in):
+            for z in board:
+                print (z)
+
+print_board(board)
