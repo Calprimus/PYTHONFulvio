17d0519af02ebc2a2548e44e106e3a75f1cd8ac6
Calprimus <fulvio.famularo@hotmail.com>
2018-04-27 20:02:36 +0200

    friday 2

--- python-3-playlist/Battleship.py
+++ python-3-playlist/Battleship.py
@@ -1,12 +1,27 @@
+from random import randint 
+
 board = []
 
 for n in range(5):
     board.append(["O"]*5)
+
 for z in board:
     for y in z:
         def print_board(board_in):
             # those are the rows
             for z in board:
                 print(" ".join(z))
+
+def random_row(board_in):
+    return randint(0, len(board_in) - 1)
+
+def random_col(board_in):
+    return randint(0, len(board_in) - 1)
+
+random_col(board)
+random_row(board)
+
+print(random_col(board))
+print(random_row(board))
 
 print_board(board)
