48a38b1c95ad7a4691aa1a03b7e6df74dd87e8c6
Calprimus <fulvio.famularo@hotmail.com>
2018-04-27 19:42:09 +0200

    friday 1

--- python-3-playlist/Battleship.py
+++ python-3-playlist/Battleship.py
@@ -5,7 +5,8 @@ for n in range(5):
 for z in board:
     for y in z:
         def print_board(board_in):
+            # those are the rows
             for z in board:
-                print (z)
+                print(" ".join(z))
 
 print_board(board)
