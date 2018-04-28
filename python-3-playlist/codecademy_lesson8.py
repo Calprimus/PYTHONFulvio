07ef5f651a26eb2160a99ff6a4011485aa4ad933
Calprimus <fulvio.famularo@hotmail.com>
2018-04-28 18:03:23 +0200

    Lesson 8 - 4/19

--- python-3-playlist/codecademy_lesson8.py
+++ python-3-playlist/codecademy_lesson8.py
@@ -7,8 +7,21 @@
 #   print ("Hello, I am a while and count is", count)
 #   count += 1
 
-loop_condition = True
+# loop_condition = True
 
-while loop_condition:
-  print("I am a loop")
-  loop_condition = False
+# while loop_condition:
+#   print("I am a loop")
+#   loop_condition = False
+
+# num = 1
+
+# while num <= 10:  # Fill in the condition
+#   # Print num squared
+#     print(num**2)
+#     num +=1
+#   # Increment num (make sure to do this!)
+
+choice = input('Enjoying the course? (y/n)')
+
+while choice is not "y" and choice is not "n":  # Fill in the condition (before the colon)
+  choice = input("Sorry, I didn't catch that. Enter again: ")
