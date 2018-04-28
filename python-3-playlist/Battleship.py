df814e9be309637a64e1863d004fc05b13809b01
Calprimus <fulvio.famularo@hotmail.com>
2018-04-28 11:37:58 +0200

    Battleship 11/19

--- python-3-playlist/Battleship.py
+++ python-3-playlist/Battleship.py
@@ -21,15 +21,25 @@ def random_col(board_in):
 ship_row = random_row(board)
 ship_col = random_col(board)
 
+# print_board(board)
+
+
+# print(random_row(board))
+# print(random_col(board))
+
 print (ship_row)
 print (ship_col)
 
 guess_row = int(input("Guess Row: "))
 guess_col = int(input("Guess Col: "))
 
+if guess_row == ship_row and guess_col == ship_col:
+    print ("Congratulations! You sank my battleship")
+else: 
+    print ("You missed my battleship!") 
+    board[ship_row][ship_col] = "X"
+    print_board(board)
 
-print(random_row(board))
-print(random_col(board))
 
 
-print_board(board)
+
