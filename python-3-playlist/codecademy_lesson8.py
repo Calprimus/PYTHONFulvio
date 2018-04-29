2d3cda41fa6bc4337ce49af288aa99250ed9657d
Calprimus <fulvio.famularo@hotmail.com>
2018-04-29 11:24:23 +0200

    Lesson 8 - 12/19

--- python-3-playlist/codecademy_lesson8.py
+++ python-3-playlist/codecademy_lesson8.py
@@ -101,14 +101,28 @@
 # print (hobbies)
 
 # >>>> 11/19
-thing = "spam!"
+# thing = "spam!"
 
-for c in thing:
-  print (c)
+# for c in thing:
+#   print (c)
 
-word = "eggs!"
+# word = "eggs!"
 
-# Your code here!
-for w in word:
-  print (w)
+# # Your code here!
+# for w in word:
+#   print (w)
+
+# >>>> 12/19
+# phrase = "A bird in the hand..."
+
+# # Add your for loop
+# for char in phrase:
+#   if char == "A" or char == "a":
+#     char == "X"
+#     print ("X", end="")
+#   else:
+#     print (char, end="") 
+
+# >>>> 13/19
+
 
