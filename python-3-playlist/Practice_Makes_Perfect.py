5aa13a5bcf3712b3fb5e82e28dcf5a4b42d3b7d4
Calprimus <fulvio.famularo@hotmail.com>
2018-05-01 18:01:32 +0200

    Practice 9/15 Final

--- python-3-playlist/Practice_Makes_Perfect.py
+++ python-3-playlist/Practice_Makes_Perfect.py
@@ -80,3 +80,13 @@ score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
          "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
          "x": 8, "z": 10}   
+
+def scrabble_score(word):
+    total = 0
+    for x in word:
+        for s in score:
+            if x.lower() == s:
+                total +=score[s]
+    return total
+
+print(scrabble_score("bebe"))
