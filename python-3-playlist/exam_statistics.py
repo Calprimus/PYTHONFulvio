8cebe6d95ed44a0b70198e069ed06220ca08cca4
Calprimus <fulvio.famularo@hotmail.com>
2018-05-02 21:01:20 +0200

    Exam 5/9

--- python-3-playlist/exam_statistics.py
+++ python-3-playlist/exam_statistics.py
@@ -8,14 +8,31 @@
 # print_grades(grades)
 
 # >>>> 4/9
-grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+# grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
-def grades_sum(scores):
-    total = 0
-    for x in scores:
-        total += x
-    return total
+# def grades_sum(scores):
+#     total = 0
+#     for x in scores:
+#         total += x
+#     return total
 
-print(grades_sum(grades))
+# print(grades_sum(grades))
+
+# >>>> 5/9
+# grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+# def grades_sum(scores):
+#     total = 0
+#     for x in scores:
+#         total += x
+#     return total
+
+# def grades_average(grades_input):
+#     aver = grades_sum(grades_input) / float(len(grades_input))
+#     return aver
+
+# print(grades_average(grades))
+
+# >>>> 7/9
 
 
