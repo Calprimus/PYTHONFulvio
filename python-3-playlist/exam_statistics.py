8e23cfe045cc495cd42727f652a3e265e76109e5
Calprimus <fulvio.famularo@hotmail.com>
2018-05-02 21:13:40 +0200

    Exam 8/9

--- python-3-playlist/exam_statistics.py
+++ python-3-playlist/exam_statistics.py
@@ -34,6 +34,33 @@
 # print(grades_average(grades))
 
 # >>>> 7/9
+# grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+# def print_grades(grades_input):
+#   for grade in grades_input:
+#     print(grade)
+
+# def grades_sum(scores):
+#   total = 0
+#   for score in scores: 
+#     total += score
+#   return total
+    
+# def grades_average(grades_input):
+#   sum_of_grades = grades_sum(grades_input)
+#   average = sum_of_grades / float(len(grades_input))
+#   return average
+
+# def grades_variance(scores):
+#     average = grades_average(scores)
+#     variance = 0
+#     for score in scores:
+#         variance += (average - score) ** 2
+#     return variance / len(scores)    
+
+# print(grades_variance(grades))
+
+# >>>> 8/9
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
 def print_grades(grades_input):
@@ -58,6 +85,10 @@ def grades_variance(scores):
         variance += (average - score) ** 2
     return variance / len(scores)    
 
-print(grades_variance(grades))
+def grades_std_deviation(variance):
+    return variance**0.5
 
-# >>>> 8/9
+variance = grades_variance(grades)    
+
+print(grades_std_deviation(variance))
+
