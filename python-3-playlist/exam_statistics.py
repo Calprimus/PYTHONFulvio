fded7d17c0ace185d75bd91c0626a01cfa84d9bc
Calprimus <fulvio.famularo@hotmail.com>
2018-05-02 20:43:35 +0200

    Exam 2/9

---
+++ python-3-playlist/exam_statistics.py
@@ -0,0 +1,8 @@
+# >>>> 2/9
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def print_grades(grades_input):
+    for x in grades_input:
+        print(x)
+
+print_grades(grades)
