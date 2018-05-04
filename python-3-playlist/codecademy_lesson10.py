060e0b3703045cf3538c9727a800c4bf03f08970
Calprimus <fulvio.famularo@hotmail.com>
2018-05-04 21:43:00 +0200

    Advance 7/18a

--- python-3-playlist/codecademy_lesson10.py
+++ python-3-playlist/codecademy_lesson10.py
@@ -95,5 +95,11 @@
 # print (even_squares)
 
 # >>>> 6/18
-cubes_by_four = [x ** 3 for x in range(1, 11) if (x ** 3) % 4 == 0]
-print (cubes_by_four)
+# cubes_by_four = [x ** 3 for x in range(1, 11) if (x ** 3) % 4 == 0]
+# print (cubes_by_four)
+
+# >>>> 7/18
+l = [i ** 2 for i in range(1, 11)]
+# Should be [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+print (l[2:9:2])
