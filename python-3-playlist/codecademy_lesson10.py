6f6cc6d21b7a4e21ad6ca2b8a9dcbec86fce0f35
Calprimus <fulvio.famularo@hotmail.com>
2018-05-05 20:06:53 +0200

    Advanced 15/18

--- python-3-playlist/codecademy_lesson10.py
+++ python-3-playlist/codecademy_lesson10.py
@@ -186,3 +186,10 @@
 # print(list(filter(lambda x: x >= 30 and x <=70 , squares)))
 
 # >>>> 15/18
+movies = {
+  "Monty Python and the Holy Grail": "Great",
+  "Monty Python's Life of Brian": "Good",
+  "Monty Python's Meaning of Life": "Okay"
+}
+
+print(movies.items())
