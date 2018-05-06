59dac3571766ddea99c46aa345ff7ad6efa2f21e
Calprimus <fulvio.famularo@hotmail.com>
2018-05-06 11:10:47 +0200

    Bits 10 - 3/14

--- python-3-playlist/codecademy_lesson10bis.py
+++ python-3-playlist/codecademy_lesson10bis.py
@@ -7,13 +7,31 @@
 # print (~88)     # Bitwise NOT
 
 # >>>> 2/14
-print(0b1, end = " ")
-print (0b10, end = " ")
-print (0b11, end = " ")
-print (0b100, end = " ")
-print (0b101, end = " ")
-print (0b110, end = " ")
-print (0b111)
-print ("******")
-print (0b1 + 0b11)
-print (0b11 * 0b11)
+# print(0b1, end = " ")
+# print (0b10, end = " ")
+# print (0b11, end = " ")
+# print (0b100, end = " ")
+# print (0b101, end = " ")
+# print (0b110, end = " ")
+# print (0b111)
+# print ("******")
+# print (0b1 + 0b11)
+# print (0b11 * 0b11)
+
+# 8's bit  4's bit  2's bit  1's bit
+#     1       0       1      0 
+#     8   +   0    +  2   +  0  = 10 
+
+# >>>> 3/14
+2 ** 0 = 1
+2 ** 1 = 2
+2 ** 2 = 4
+2 ** 3 = 8
+2 ** 4 = 16
+2 ** 5 = 32
+2 ** 6 = 64
+2 ** 7 = 128
+2 ** 8 = 256
+2 ** 9 = 512
+2 ** 10 = 1024
+
