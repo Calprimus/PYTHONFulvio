ae66d4ba186e36c51db32fee37712075c7aadcc6
Calprimus <fulvio.famularo@hotmail.com>
2018-05-08 21:08:02 +0200

    Classes A 14/18

--- python-3-playlist/codecademy_lesson11_Classes1.py
+++ python-3-playlist/codecademy_lesson11_Classes1.py
@@ -216,21 +216,47 @@
 # # Hello, Emily
 # ceo.greet(emp)
 # # Get back to work, Steve!
+# -----------------------------------------------------
+# class Employee(object):
+#     """Models real-life employees!"""
+#     def __init__(self, employee_name):
+#         self.employee_name = employee_name
 
-class Employee(object):
-    """Models real-life employees!"""
-    def __init__(self, employee_name):
-        self.employee_name = employee_name
+#     def calculate_wage(self, hours):
+#         self.hours = hours
+#         return hours * 20.00
 
-    def calculate_wage(self, hours):
-        self.hours = hours
-        return hours * 20.00
+# # Add your code below!
 
-# Add your code below!
+# class PartTimeEmployee(Employee):
+#     def calculate_wage(self, hours):
+#         self.hours = hours
+#         return hours * 12.00
+#-------------------------------------------------------
 
-class PartTimeEmployee(Employee):
-    def calculate_wage(self, hours):
-        self.hours = hours
-        return hours * 12.00
 
 # >>>> 14/18
+# class Employee(object):
+#     """Models real-life employees!"""
+#     def __init__(self, employee_name):
+#         self.employee_name = employee_name
+
+#     def calculate_wage(self, hours):
+#         self.hours = hours
+#         return hours * 20.00
+
+# # Add your code below!
+
+# class PartTimeEmployee(Employee):
+#     def calculate_wage(self, hours):
+#         self.hours = hours
+#         return hours * 12.00
+    
+#     def full_time_wage(self, hours):
+#         return super(PartTimeEmployee, self).calculate_wage(hours)
+
+# party = PartTimeEmployee("milton")
+# print(party.full_time_wage(10))
+
+# >>>> 15/18
+
