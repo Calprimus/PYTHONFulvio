3d3a00c256030a85edf9f6015593d1b84f0c7aee
Calprimus <fulvio.famularo@hotmail.com>
2018-05-08 11:37:04 +0200

    Classes A 5/18

--- python-3-playlist/codecademy_lesson11_Classes1.py
+++ python-3-playlist/codecademy_lesson11_Classes1.py
@@ -58,3 +58,11 @@
 #     self.name = name
 
 # >>>> 5/18
+class Animal(object):
+      def __init__(self, name):
+          self.name = name
+
+zebra = Animal("Jeffrey")
+print (zebra.name)
+
+# >>>> 6/18
