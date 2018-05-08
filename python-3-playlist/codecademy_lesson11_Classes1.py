d94c5ff55ee2b614183428ef6105bcd886531c4b
Calprimus <fulvio.famularo@hotmail.com>
2018-05-08 20:41:15 +0200

    Classes A 13/18

--- python-3-playlist/codecademy_lesson11_Classes1.py
+++ python-3-playlist/codecademy_lesson11_Classes1.py
@@ -200,4 +200,37 @@
 #         self.side3=side3
 
 # >>>> 13/18
+# class Employee(object):
+#       def __init__(self, name):
+#     self.name = name
+#   def greet(self, other):
+#     print "Hello, %s" % other.name
 
+# class CEO(Employee):
+#   def greet(self, other):
+#     print "Get back to work, %s!" % other.name
+
+# ceo = CEO("Emily")
+# emp = Employee("Steve")
+# emp.greet(ceo)
+# # Hello, Emily
+# ceo.greet(emp)
+# # Get back to work, Steve!
+
+class Employee(object):
+    """Models real-life employees!"""
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 20.00
+
+# Add your code below!
+
+class PartTimeEmployee(Employee):
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 12.00
+
+# >>>> 14/18
