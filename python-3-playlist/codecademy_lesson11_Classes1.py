1f0b9df25ec6d07444850da903896a1af18d4003
Calprimus <fulvio.famularo@hotmail.com>
2018-05-08 21:33:15 +0200

    Classes A 17/18

--- python-3-playlist/codecademy_lesson11_Classes1.py
+++ python-3-playlist/codecademy_lesson11_Classes1.py
@@ -267,6 +267,21 @@
 
 # >>>> 16/18
 
+# class Triangle(object):
+#      number_of_sides = 3
+#      def __init__(self, angle1, angle2, angle3):
+#         self.angle1=angle1
+#         self.angle2=angle2
+#         self.angle3=angle3
+
+       
+#      def check_angles(self):
+#         if (self.angle1 + self.angle2 + self.angle3) == 180:
+#             return True
+#         else:
+#             return False 
+
+# >>>> 17/18
 class Triangle(object):
      number_of_sides = 3
      def __init__(self, angle1, angle2, angle3):
@@ -281,5 +296,8 @@ class Triangle(object):
         else:
             return False 
 
-# >>>> 17/18
+my_triangle = Triangle(90, 30, 60)
+print(my_triangle.number_of_sides)
+print(my_triangle.check_angles())
 
+# >>>> 18/18
