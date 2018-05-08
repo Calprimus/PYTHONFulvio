657b0a90faa64a77ad3bc82d8bdf60963e40c3d3
Calprimus <fulvio.famularo@hotmail.com>
2018-05-08 20:29:18 +0200

    Classes A 11/18

--- python-3-playlist/codecademy_lesson11_Classes1.py
+++ python-3-playlist/codecademy_lesson11_Classes1.py
@@ -141,30 +141,46 @@
 # ocelot.description()
 
 # >>>> 10/18
-class ShoppingCart(object):
-    def __init__(self, customer_name):
-        """Creates shopping cart objects
-        for users of our fine website."""
-        self.customer_name = customer_name
-        self.items_in_cart = {}
+# class ShoppingCart(object):
+#     def __init__(self, customer_name):
+#         """Creates shopping cart objects
+#         for users of our fine website."""
+#         self.customer_name = customer_name
+#         self.items_in_cart = {}
 
-    def add_item(self, product, price):
-        """Add product to the cart."""
-        if not product in self.items_in_cart:
-            self.items_in_cart[product] = price
-            print (product + " added.")
-        else:
-            print (product + " is already in the cart.")
+#     def add_item(self, product, price):
+#         """Add product to the cart."""
+#         if not product in self.items_in_cart:
+#             self.items_in_cart[product] = price
+#             print (product + " added.")
+#         else:
+#             print (product + " is already in the cart.")
 
-    def remove_item(self, product):
-        """Remove product from the cart."""
-        if product in self.items_in_cart:
-            del self.items_in_cart[product]
-            print (product + " removed.")
-        else:
-            print (product + " is not in the cart.")
+#     def remove_item(self, product):
+#         """Remove product from the cart."""
+#         if product in self.items_in_cart:
+#             del self.items_in_cart[product]
+#             print (product + " removed.")
+#         else:
+#             print (product + " is not in the cart.")
 
-my_cart = ShoppingCart("Fulvio")
-my_cart.add_item("Bread", 23)
+# my_cart = ShoppingCart("Fulvio")
+# my_cart.add_item("Bread", 23)
 
 # >>>> 11/18
+class Customer(object):
+    """Produces objects that represent customers."""
+    def __init__(self, customer_id):
+        self.customer_id = customer_id
+
+    def display_cart(self):
+        print ("I'm a string that stands in for the contents of your shopping cart!")
+
+class ReturningCustomer(Customer):
+    """For customers of the repeat variety."""
+    def display_order_history(self):
+        print ("I'm a string that stands in for your order history!")
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
