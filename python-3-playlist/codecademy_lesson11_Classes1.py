4a7e54202881149d7aee828c03dbca18eb534083
Calprimus <fulvio.famularo@hotmail.com>
2018-05-08 11:15:48 +0200

    Classes A 2/18

--- python-3-playlist/codecademy_lesson11_Classes1.py
+++ python-3-playlist/codecademy_lesson11_Classes1.py
@@ -21,26 +21,28 @@
 # lemon.description()
 # lemon.is_edible()
 
-class Fruit(object):
-    """A class that makes various tasty fruits."""
-    def __init__(self, name, color, flavor, poisonous):
-        self.name = name
-        self.color = color
-        self.flavor = flavor
-        self.poisonous = poisonous
+# class Fruit(object):
+#     """A class that makes various tasty fruits."""
+#     def __init__(self, name, color, flavor, poisonous):
+#         self.name = name
+#         self.color = color
+#         self.flavor = flavor
+#         self.poisonous = poisonous
 
-    def description(self):
-        print ("I'm a %s %s and I taste %s." % (self.color, self.name, self.flavor))
+#     def description(self):
+#         print ("I'm a %s %s and I taste %s." % (self.color, self.name, self.flavor))
 
-    def is_edible(self):
-        if not self.poisonous:
-            print ("Yep! I'm edible.")
-        else:
-            print ("Don't eat me! I am super poisonous.")
+#     def is_edible(self):
+#         if not self.poisonous:
+#             print ("Yep! I'm edible.")
+#         else:
+#             print ("Don't eat me! I am super poisonous.")
 
-lemon = Fruit("lemon", "yellow", "sour", False)
+# lemon = Fruit("lemon", "yellow", "sour", False)
 
-lemon.description()
-lemon.is_edible()
+# lemon.description()
+# lemon.is_edible()
 
 # >>>> 2/18
+class Animal(object):
+  pass
