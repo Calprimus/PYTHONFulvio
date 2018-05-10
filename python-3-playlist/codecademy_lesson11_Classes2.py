7d28352d6693d6d87d5bbaf5c7f4ae0c1fa41732
Calprimus <fulvio.famularo@hotmail.com>
2018-05-10 21:21:30 +0200

    Classes B 7/11

--- python-3-playlist/codecademy_lesson11_Classes2.py
+++ python-3-playlist/codecademy_lesson11_Classes2.py
@@ -36,6 +36,20 @@
 # print(my_car.model)
 
 # >>>> 6/11
+# class Car(object):
+#     condition = "new"
+#     def __init__(self, model, color, mpg):
+#         self.model = model
+#         self.color = color
+#         self.mpg = mpg
+
+# my_car = Car("DeLorean", "silver", 88)
+
+# print(my_car.model)
+# print(my_car.color)
+# print(my_car.mpg)
+
+# >>>> 7/11
 class Car(object):
     condition = "new"
     def __init__(self, model, color, mpg):
@@ -43,11 +57,13 @@ class Car(object):
         self.color = color
         self.mpg = mpg
 
+    def drive_car(self):
+        self.condition = "used"
+
 my_car = Car("DeLorean", "silver", 88)
 
-print(my_car.model)
-print(my_car.color)
-print(my_car.mpg)
+print(my_car.condition)
+my_car.drive_car()
+print(my_car.condition)
 
-# >>>> 7/11
 
