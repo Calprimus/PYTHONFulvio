be3474fdc6f546d0e41a9b29f8aaa48ea60884a9
Calprimus <fulvio.famularo@hotmail.com>
2018-05-10 21:41:52 +0200

    Classes B 8/11

--- python-3-playlist/codecademy_lesson11_Classes2.py
+++ python-3-playlist/codecademy_lesson11_Classes2.py
@@ -50,12 +50,30 @@
 # print(my_car.mpg)
 
 # >>>> 7/11
+# class Car(object):
+#     condition = "new"
+#     def __init__(self, model, color, mpg):
+#         self.model = model
+#         self.color = color
+#         self.mpg = mpg
+#     def display_car(self):
+#         return "This is a " + self.color + " "+ self.model + " with " + str(self.mpg) + " MPG"
+        
+# my_car = Car("DeLorean", "silver", 88)
+
+# print(my_car.display_car())
+
+# >>>> 8/11
+
 class Car(object):
     condition = "new"
     def __init__(self, model, color, mpg):
         self.model = model
         self.color = color
         self.mpg = mpg
+
+    def display_car(self):
+        return "This is a " + self.color + " "+ self.model + " with " + str(self.mpg) + " MPG"
 
     def drive_car(self):
         self.condition = "used"
@@ -65,5 +83,3 @@ my_car = Car("DeLorean", "silver", 88)
 print(my_car.condition)
 my_car.drive_car()
 print(my_car.condition)
-
-
