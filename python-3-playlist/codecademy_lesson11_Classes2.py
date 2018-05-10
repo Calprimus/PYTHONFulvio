eafa9a2d03bf7b621285076285147e8727d6690d
Calprimus <fulvio.famularo@hotmail.com>
2018-05-10 22:24:13 +0200

    Classes B 11/11

--- python-3-playlist/codecademy_lesson11_Classes2.py
+++ python-3-playlist/codecademy_lesson11_Classes2.py
@@ -128,36 +128,16 @@
 # print(my_car.condition)
 
 # >>>> 11/11
-class Car(object):
-    condition = "new"
-    def __init__(self, model, color, mpg):
-        self.model = model
-        self.color = color
-        self.mpg = mpg
 
-    def display_car(self):
-        return "This is a " + self.color + " "+ self.model + " with " + str(self.mpg) + " MPG"
+class Point3D(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def __repr__(self):
+        return "(%d, %d, %d)" % (self.x, self.y, self.z)
 
-    def drive_car(self):
-        self.condition = "used"
+my_point = Point3D(1, 2, 3)
+print(my_point)
 
-my_car = Car("DeLorean", "silver", 88)
-
-print(my_car.condition)
-my_car.drive_car()
-print(my_car.condition)
-
-class ElectricCar(Car):
-    def __init__(self, battery_type, model, color, mpg):
-        self.battery_type = battery_type
-        self.model = model
-        self.color = color
-        self.mpg = mpg
-    def drive_car(self):
-        self.condition = "like new"
-
-my_car = ElectricCar("molten salt", "ferrari", "red", 200)
-
-print(my_car.condition)
-my_car.drive_car()
-print(my_car.condition)
