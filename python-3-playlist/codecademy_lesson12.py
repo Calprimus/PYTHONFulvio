566649abf383f34faa860ae599c2a22cdd7add51
Calprimus <fulvio.famularo@hotmail.com>
2018-05-12 18:52:38 +0200

    File I/O 3/9

--- python-3-playlist/codecademy_lesson12.py
+++ python-3-playlist/codecademy_lesson12.py
@@ -1,16 +1,24 @@
 # >>>> 1/9
-my_list = [i ** 2 for i in range(1, 11)]
-# Generates a list of squares of the numbers 1 - 10
+# my_list = [i ** 2 for i in range(1, 11)]
+# # Generates a list of squares of the numbers 1 - 10
 
-f = open("output.txt", "w")
+# f = open("output.txt", "w")
 
-for item in my_list:
-  f.write(str(item) + "\n")
+# for item in my_list:
+#   f.write(str(item) + "\n")
 
-f.close()
+# f.close()
 
 # >>>> 2/9
-my_file = open("output.txt", "r+")
+# my_file = open("output.txt", "r+")
 
 # >>>> 3/9
+my_list = [i ** 2 for i in range(1, 11)]
 
+my_file = open("output.txt", "r+")
+
+for item in my_list:
+  my_file.write(str(item) + "\n")
+
+
+my_file.close()
