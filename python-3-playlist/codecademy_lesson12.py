f5839b995d5356a4d07f30cd9a2524b211e1ab73
Calprimus <fulvio.famularo@hotmail.com>
2018-05-13 11:53:34 +0200

    File I/O 6/9

--- python-3-playlist/codecademy_lesson12.py
+++ python-3-playlist/codecademy_lesson12.py
@@ -39,3 +39,19 @@ print(my_file.readline())
 
 my_file.close()
 
+# >>>> 6/9
+# Use a file handler to open a file for writing
+write_file = open("text.txt", "w")
+
+
+# Open the file for reading
+read_file = open("text.txt", "r")
+
+# Write to the file
+write_file.write("Not closing files is VERY BAD.")
+write_file.close()
+
+
+# Try to read from the file
+print (read_file.read())
+read_file.close()
